de4fd2931310055f41785968547da4ae10589bd4
Seungbin Song <seungbin@corelab.or.kr>
2019-07-02 21:56:30 +0900

    Add expression converter

---
+++ expressionConverter.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Seungbin Song
+ */
+
+#include "expressionConverter.h"
+
+namespace PSDN {
+
+std::string* ExpressionConverter::convert(const IR::Expression* e) {
+  e->apply(*this);
+  auto result = ::get(map, e->to<IR::Expression>());
+  if (result == nullptr)
+    BUG("%1%: Cannot convert expression", e);
+  return result;
+};
+
+void ExpressionConverter::mapExpression(const IR::Expression* e, std::string* str) {
+  map.emplace(e, str);
+}
+
+std::string* ExpressionConverter::get(const IR::Expression* e) const {
+  auto result = ::get(map, e);
+  if (result == nullptr)
+    BUG("%1%: There is no expression mapping", e);
+  return result;
+}
+
+/// There is no boolean value in SDNet, so change boolean literals into 1 or 0.
+void ExpressionConverter::postorder(const IR::BoolLiteral* expression) {
+  std::string* str = nullptr;
+  if (*expression == IR::BoolLiteral(true))
+    str = new std::string("1");
+  else 
+    str = new std::string("0");
+  mapExpression(expression,str);
+}
+
+void ExpressionConverter::postorder(const IR::Cast* expression) {
+  std::string* str = nullptr;
+  // Get original type from expression, and destination type from destType.
+  int originalWidth = expression->expr->type->width_bits();
+  int destWidth = expression->destType->width_bits();
+  // If destination type is larger than original type, extend with zero.
+  if (destWidth > originalWidth) {
+    str = new std::string(std::to_string(destWidth-originalWidth));
+    str->append("w ++ ");
+  }
+  mapExpression(expression,str);
+}
+
+};
---
+++ expressionConverter.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2019 Seungbin Song
+ */
+
+#ifndef PSDN_EXPRESSION_CONVERTER_H
+#define PSDN_EXPRESSION_CONVERTER_H
+
+#include <string>
+
+#include "ir/ir.h"
+#include "frontends/common/resolveReferences/referenceMap.h"
+#include "frontends/p4/coreLibrary.h"
+#include "frontends/p4/typeMap.h"
+
+#include "programStructure.h"
+
+namespace PSDN {
+
+
+class ExpressionConverter : public Inspector {
+  P4::ReferenceMap* refMap;
+  P4::TypeMap* typeMap;
+  ProgramStructure* structure;
+  P4::P4CoreLibrary& coreLibrary;
+  
+  /// Save result into map
+  std::map<const IR::Expression*, std::string*> map;
+  
+  public:
+  ExpressionConverter(P4::ReferenceMap* refMap, P4::TypeMap* typeMap,
+      ProgramStructure* structure) : refMap(refMap), typeMap(typeMap),
+        structure(structure), coreLibrary(P4::P4CoreLibrary::instance),
+        singleExpressionOnly(false) {}
+
+
+  /// If true, convert only single expressions like table keys
+  bool singleExpressionOnly;
+
+  /// @return a parameter which expression refers to
+  const IR::Parameter* enclosingParamReference(const IR::Expression* e);
+
+  /**
+   * Convert an expression into string that represents an SDNet expression
+   * @param e   expression to convert
+   * 
+   */
+  std::string* convert(const IR::Expression* e);
+
+  std::string* get(const IR::Expression* e) const;
+
+	void mapExpression(const IR::Expression* e, std::string* str);
+
+	void postorder(const IR::BoolLiteral* expression) override;
+// 	void postorder(const IR::MethodCallExpression* expression) override;
+ 	void postorder(const IR::Cast* expression) override;
+// 	void postorder(const IR::Slice* expression) override;
+// 	void postorder(const IR::AddSat* expression) override;
+// 	void postorder(const IR::SubSat* expression) override;
+// 	void postorder(const IR::Constant* expression) override;
+// 	void postorder(const IR::ArrayIndex* expression) override;
+// 	void postorder(const IR::Member* expression) override;
+// 	void postorder(const IR::Mux* expression) override;
+// 	void postorder(const IR::IntMod* expression) override;
+// 	void postorder(const IR::Operation_Binary* expression) override;
+// 	void postorder(const IR::ListExpression* expression) override;
+// 	void postorder(const IR::StructInitializerExpression* expression) override;
+// 	void postorder(const IR::Operation_Unary* expression) override;
+// 	void postorder(const IR::PathExpression* expression) override;
+// 	void postorder(const IR::TypeNameExpression* expression) override;
+// 	void postorder(const IR::Expression* expression) override;
+
+};
+
+}; //namespace PSDN
+
+
+#endif
